cfd27dcf9bdd60a610b6f1f87e81e594caf0e78f
Diego Sousa <50030046+diegosousa10@users.noreply.github.com>
2020-05-19 22:46:27 -0300

    Commit do projeto inicial

---
+++ src/test/java/com/diegosousadev/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegosousadev.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
