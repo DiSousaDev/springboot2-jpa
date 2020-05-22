8c96d6b951d2adad9635618fd1faead83c35bde5
Diego Sousa <50030046+diegosousa10@users.noreply.github.com>
2020-05-21 23:37:46 -0300

    Product

---
+++ src/main/java/com/diegosousadev/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.diegosousadev.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diegosousadev.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}
